ec807aafc9c7be61eb42d74e8dcdb262bcdb9e90
Lukas Wolf <lukas.wolf.0467@student.uu.se>
2024-01-31 17:54:00 +0100

    trying to make sense and visualized the frogball patterns, player position cubes etc

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTriangle : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
-    public position
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +13,6 @@ public class PlayerTriangle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
